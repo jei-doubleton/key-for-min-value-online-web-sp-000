5240f0a4b0c77dc5fa9408d18a7140f9df36cc38
J <jennifer.singleton@gmail.com>
2019-01-18 00:30:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+#collect an array of the numbers each item
+  numbers = name_hash.collect do |item, number|
+    number
+  end
+#find out if each value is less than all numbers in array
+  name_hash.collect do |item, number|
+    i = 0
+    while i < numbers.length
+      if number < numbers[i]
+        key
+      end
+      i += 1
+    end
+  end      
 end
